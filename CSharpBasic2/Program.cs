a066d4089a4bc46ec4243655daaceeb729712092
Charles Burch <georgia_beast89@yahoo.com>
2019-03-23 20:34:12 -0500

    created person class Parse method

--- CSharpBasic2/Program.cs
+++ CSharpBasic2/Program.cs
@@ -10,6 +10,14 @@ namespace CSharpBasic2
         {
             Console.WriteLine("Hi {0} I am {1} ", to, Name);
         }
+
+        public Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+
+            return person;
+        }
     }
     class MainClass
     {   
