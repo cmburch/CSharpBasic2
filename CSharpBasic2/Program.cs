99d8a51a467bf1b68fc8ae871073185a6d7a8118
Charles Burch <georgia_beast89@yahoo.com>
2019-03-23 19:58:48 -0500

    Declaring static members

--- CSharpBasic2/Program.cs
+++ CSharpBasic2/Program.cs
@@ -3,10 +3,12 @@
 namespace CSharpBasic2
 {
     class MainClass
-    {
+    {   
+        //Declaring static members
+        public static int PeopleCount = 0;
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
