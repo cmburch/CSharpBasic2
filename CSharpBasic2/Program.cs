a7b94ed5df589f58579d24f8d462a3edf1ef9444
Charles Burch <georgia_beast89@yahoo.com>
2019-03-23 20:24:37 -0500

    created instance of person class

--- CSharpBasic2/Program.cs
+++ CSharpBasic2/Program.cs
@@ -1,14 +1,27 @@
 ﻿using System;
 
 namespace CSharpBasic2
-{
+{   
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0} I am {1} ", to, Name);
+        }
+    }
     class MainClass
     {   
         //Declaring static members
         public static int PeopleCount = 0;
 
+       
         public static void Main(string[] args)
         {
+            var person = new Person();
+            person.Name = "John";
+            person.Introduce("Max");
         }
     }
 }
